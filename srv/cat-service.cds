a0d359bbda768b70a282bf1a57566b911067b6a8
Ajay Kumar <ajay.kumar@msitekus.com>
2024-07-20 20:00:06 +0000

    Working Destination

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,12 +1,22 @@
 using zsupplier as persistence from '../db/schema';
 using {sap.common as common} from '../db/common';
+using {CE_PURCHASEORDER_0001 as external} from './external/CE_PURCHASEORDER_0001';
 
 service InvCatalogService @(requires: 'authenticated-user') {
 
-    
-    entity Invoice            as projection on persistence.InvoiceEntity;
+    entity PurchaseOrder     as
+        projection on external.PurchaseOrder {
+            *,
+            _PurchaseOrderItem : redirected to PurchaseOrderItem,
+            _PurchaseOrderPartner,
+            _SupplierAddress
+        };
 
-    entity InvoiceItem as projection on persistence.InvoiceEntity.to_InvoiceItem;
+    entity PurchaseOrderItem as projection on external.PurchaseOrderItem{
+            *
+        };
+    entity Invoice           as projection on persistence.InvoiceEntity;
+    entity InvoiceItem       as projection on persistence.InvoiceEntity.to_InvoiceItem;
 
 
 }
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -10,11 +10,35 @@ const { SELECT, INSERT, UPDATE } = cds.ql;
 class InvCatalogService extends cds.ApplicationService {
     async init() {
         const {
-            InvoiceEntity,
-            InvoiceItemEntity,
-
+            Invoice,
+            InvoiceItem,
+            PurchaseOrder,
+            PurchaseOrderItem
         } = this.entities;
         const db = await cds.connect.to("db");
+        
+        this.on('READ', [PurchaseOrder, PurchaseOrderItem], async (req) => {
+            const pos = await cds.connect.to('CE_PURCHASEORDER_0001');
+            return await pos.run(req.query);
+        });
+        
+        // this.on("READ", 'PurchaseOrder', async (req) => {
+        //     // The API Sandbox returns alot of business partners with empty names.
+        //     // We don't want them in our application
+        //     // req.query.where("LastName <> '' and FirstName <> '' ");
+        //     console.log('here');
+        //     const pos = await cds.connect.to('CE_PURCHASEORDER_0001');
+        //     const result = await pos.run(req.query);
+        //     console.log('result');
+        //     return result;
+        // });
+
+
+        // this.on('READ', 'PurchaseOrderItem', async (req) => {
+        //     const pos = await cds.connect.to('CE_PURCHASEORDER_0001');
+        //     return pos.run(req.query);
+        // });
+
 
         return super.init();
     }
