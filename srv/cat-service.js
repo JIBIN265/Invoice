6e3d46f57df2ae1bdce8bb15ba6e020a5e38cef6
Jibin Thomas <jibin.thomas@msitek.us>
2024-07-21 16:57:07 +0000

    3rd commit

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -40,28 +40,6 @@ class InvCatalogService extends cds.ApplicationService {
         // });
 
 
-
-
-        // this.on('READ', PurchaseOrder, async req => {
-        //     const pos = await cds.connect.to('CE_PURCHASEORDER_0001');
-        //     console.log(req.query);
-        //     const record2 = await pos.run(req.query);
-        //     console.log(record2);
-        //     return record2;
-        //     //return pos.run(req.query);
-        // });
-
-        this.on('doThreeWayMatch', 'Invoice', async req => {
-
-            console.log("Three way Verification Code Check");
-            const { ID } = req.params[0];
-            if (!ID) { return; }
-            const record = await db.run(SELECT.one.from(Invoice).where({ ID: ID }));
-            const recordItem = await db.run(SELECT.from(InvoiceItem).where({ UP__ID: ID }));
-            const { purchaseOrder } = recordItem[0];
-            console.log(record);
-        });
-
         return super.init();
     }
 }
